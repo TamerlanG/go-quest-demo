dafb7b83e499b52fde244e5a6966b8a80b68fa71
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2022-10-15 23:15:01 +0200

    feat: update quest functionality

--- controllers/quests.go
+++ controllers/quests.go
@@ -13,14 +13,14 @@ import (
 
 var validate *validator.Validate
 
-type CreateQuestInput struct {
+type QuestInput struct {
   Title string `json:"title" validate:"required"`
   Description string `json:"description" validate:"required"`
   Reward int `json:"reward" validate:"required"`
 }
 
 func CreateQuest(w http.ResponseWriter, r *http.Request){
-  var input CreateQuestInput 
+  var input QuestInput 
 
   body, _ := ioutil.ReadAll(r.Body)
   _ = json.Unmarshal(body, &input)
@@ -85,3 +85,35 @@ func DeleteQuest(w http.ResponseWriter, r *http.Request){
 }
 
 
+func UpdateQuest(w http.ResponseWriter, r *http.Request){
+  w.Header().Set("Content-Type", "application/json")
+
+  id := mux.Vars(r)["id"]
+  var quest models.Quest
+
+  if err := models.DB.Where("id = ?", id).First(&quest).Error; err != nil{
+    utils.RespondWithError(w, http.StatusNotFound, "Quest not found")
+    return
+  }
+
+  var input QuestInput 
+
+  body, _ := ioutil.ReadAll(r.Body)
+  _ = json.Unmarshal(body, &input)
+
+  validate = validator.New()
+  err := validate.Struct(input)
+
+  if err != nil {
+    utils.RespondWithError(w, http.StatusBadRequest, "Validation Error")
+    return 
+  }
+  
+  quest.Title = input.Title
+  quest.Description = input.Description
+  quest.Reward = input.Reward
+
+  models.DB.Save(&quest)
+
+  json.NewEncoder(w).Encode(quest)
+}
--- controllers/setup.go
+++ controllers/setup.go
@@ -9,10 +9,12 @@ import (
 func New() http.Handler {
   router := mux.NewRouter()
 
-  router.HandleFunc("/quest", CreateQuest).Methods("POST") 
-  router.HandleFunc("/quest/{id}",GetQuest).Methods("GET")
-  router.HandleFunc("/quest/{id}", DeleteQuest).Methods("DELETE")
+
   router.HandleFunc("/quests", GetAllQuests).Methods("GET")
+  router.HandleFunc("/quest/{id}",GetQuest).Methods("GET")
+  router.HandleFunc("/quest", CreateQuest).Methods("POST") 
+  router.HandleFunc("/quest/{id}", UpdateQuest).Methods("PUT")
+  router.HandleFunc("/quest/{id}", DeleteQuest).Methods("DELETE")
   
   
   return router
