e246f828628c4e03800a3b7eee4583913174d861
Tamerlan Gudabayev <tamerlanalbehairy@gmail.com>
2022-10-15 19:07:35 +0200

    feat: add timestamp fields

--- models/quest.go
+++ models/quest.go
@@ -1,10 +1,15 @@
 package models
 
+import "time"
+
 type Quest struct {
   ID uint `json:"id" gorm:"primary_key"`  
   Title string `json:"title"`
   Description string `json:"description"`
   Reward int `json:"reward"`
+  CreatedAt time.Time `json:"created_at"`
+  UpdatedAt time.Time `json:"updated_at"`
+
 }
 
 func NewQuest(title string, description string, reward int) (quest *Quest, err error){
